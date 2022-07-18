4e59b0ad6070320c6e23584d15a7d4bddb3b1b33
Austin Longo <austinlongocontracting@gmail.com>
2022-07-17 22:52:49 -0400

    commit tree 4b84b1536c13df0d853c07eb390ca69c2c956311

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,30 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
-}
+  
+  const DirectorList = directors.map((director) => {
+
+    const DirectorMoviesList = director.movies.map((movie) => {
+      return (<li key={movie}>{movie}</li>)
+    })
+
+    return (
+      <div key={director.name}>
+        {director.name}
+        <ul>
+          {DirectorMoviesList}
+        </ul>
+      </div>
+    )
+  })
+  
+  
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {DirectorList}
+
+    </div>
+)}
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,31 @@
 import React from "react";
 import { movies } from "../data";
 
-function Movies() {
-  return <div>{/*{code here}*/}</div>;
+const MovieList = movies.map((movie) => {
+  
+  const GenreList = movie.genres.map((genre) => {
+    return (
+    <li key={genre}>{genre}</li>
+    )})
+  
+  return(
+  <div key={movie.title}>
+    <h3>{movie.title}</h3>
+    <p>{movie.time}</p>
+    <ul>{GenreList}</ul>
+  </div>
+  )
 }
+)
+
+
+function Movies() {
+  return (
+  <div> 
+    <h1>Movies Page</h1>
+    <div>{MovieList}</div>
+
+  </div>
+)}
 
 export default Movies;
